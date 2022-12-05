301e9cc63020b80510ca8d0a7645e980dbed6544
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2022-12-05 23:10:53 +0200

    feat: add fs/rename solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const errorText = 'FS operation failed';
+
+    try {
+        await fs.rename('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md');
+    } catch(error) {
+        throw new Error(errorText);
+    }
 };
 
 await rename();

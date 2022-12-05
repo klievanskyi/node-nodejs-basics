4a0dbc345054d2f908f725b723972082c71374d8
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2022-12-05 23:40:56 +0200

    feat: add fs/read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const errorText = 'FS operation failed';
+
+    try {
+        const contents = await readFile('./src/fs/files/fileToRead.txt', { encoding: 'utf8' });
+        console.log(contents);
+    } catch(error) {
+        throw new Error(errorText);
+    }
 };
 
 await read();

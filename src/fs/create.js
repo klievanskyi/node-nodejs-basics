413cc46a895c7748da5663707650568b038ce830
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2022-12-05 21:54:04 +0200

    feat: add fs/create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const text = 'I am fresh and young';
+    const errorText = 'FS operation failed';
+    const path = './src/fs/files/fresh.txt';
+
+    try {
+        writeFile(path, text, {flag: 'wx+'});
+    } catch(error) {
+        throw new Error(errorText);
+    }
+
 };
 
 await create();

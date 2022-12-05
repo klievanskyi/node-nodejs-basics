fcd8d86db220e955ad4f540ce804d56db045cdf9
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2022-12-05 22:53:57 +0200

    feat: add fs/copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const errorText = 'FS operation failed';
+
+    try {
+        const files = await readdir('./src/fs/files/');
+        await mkdir('./src/fs/files_copy/', { recursive: false });
+        for (const file of files) {
+            copyFile(`./src/fs/files/${file}`, `./src/fs/files_copy/${file}`);
+        }
+    } catch(error) {
+        throw new Error(errorText);
+    }
 };
 
 await copy();
